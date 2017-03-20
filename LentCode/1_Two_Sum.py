7ec6dce05c3643c5ec57a491566bb4cbd5dd8d9a
YangXuLei <yangxulei84@qq.com>
2017-03-20 19:06:17 +0800

    LeetCode1，two sum

---
+++ LentCode/1_Two_Sum.py
@@ -0,0 +1,15 @@
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        #hash用于建立数值到下标的映射
+        hash = {}
+        #循环nums数值，并添加映射
+        for i in range(len(nums)):
+            if target - nums[i] in hash:
+                return [hash[target - nums[i]], i]
+            hash[nums[i]] = i
+        #无解的情况
+        return [-1, -1]
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# arithmetic
+# arithmetic/
+包含LeetCode算法和《算法导论》课后题
 #文章
 [新的快速排序算法: 《Dual-Pivot QuickSort》阅读笔记](http://www.jianshu.com/p/2c6f79e8ce6e)
 #资源
